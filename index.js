ec79163a736e23ed0cdfcc4432167fafc7877125
KM <info@smakintel.com>
2024-04-10 21:07:33 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,91 @@
+/* eslint-disable no-console */
+
+import { unixfs } from '@helia/unixfs'
+import { FsBlockstore } from 'blockstore-fs'
+import { FsDatastore } from 'datastore-fs'
+import { createHelia } from 'helia'
+import { createLibp2p } from 'libp2p'
+import { tcp } from '@libp2p/tcp' 
+import { noise } from '@chainsafe/libp2p-noise' // for encrypting connections 
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { bootstrap } from '@libp2p/bootstrap'
+import { identify } from '@libp2p/identify'
+// Block Store to store DHT data
+const blockstore = new FsBlockstore('block-store')
+const datastore = new FsDatastore('data-store')
+
+
+// libp2p is the networking layer that underpins Helia
+const libp2p = await createLibp2p({
+  datastore,
+  blockstore,
+  addresses: {
+    listen: [
+      // add a listen address (localhost) to accept TCP connections on a random port
+      '/ip4/127.0.0.1/tcp/0'
+    ]
+  },
+  transports: [
+    tcp()
+  ],
+  connectionEncryption: [
+    noise()
+  ],
+  streamMuxers: [
+    yamux()
+  ],
+  peerDiscovery: [
+    bootstrap({
+      list: [
+          '/ip4/13.229.133.138/tcp/4001/p2p/12D3KooWR6PtoaHyoQZ6tJmjEdGHCNA4LMXctrvnph4DNYYLPprb'
+          ]
+    })
+  ],
+  services: {
+    identify: identify()
+  }
+})
+
+// create a Helia node
+const helia = await createHelia({
+    datastore,
+    blockstore   
+
+
+})
+
+// create a filesystem on top of Helia, in this case it's UnixFS
+const fs = unixfs(helia)
+
+// we will use this TextEncoder to turn strings into Uint8Arrays
+const encoder = new TextEncoder()
+
+// add the bytes to your node and receive a unique content identifier
+const cid = await fs.addBytes(encoder.encode('Hello World 201'))
+
+console.log('Added file:', cid.toString())
+
+// create a second Helia node using the same blockstore
+const helia2 = await createHelia({
+  libp2p,
+    datastore,
+  blockstore
+})
+
+console.log(`Node started with id ${libp2p.peerId.toString()}`)
+
+// create a second filesystem
+const fs2 = unixfs(helia2)
+
+// this decoder will turn Uint8Arrays into strings
+const decoder = new TextDecoder()
+let text = ''
+
+// read the file from the blockstore using the second Helia node
+for await (const chunk of fs2.cat(cid)) {
+  text += decoder.decode(chunk, {
+    stream: true
+  })
+}
+
+console.log('Added file contents:', text)
