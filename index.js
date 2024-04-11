8b8d5fd8bc52752ac97f7be713f37bee55c29858
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-04-11 11:17:39 -0700

    fix: stop node, cleanup indents

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import { noise } from '@chainsafe/libp2p-noise' // for encrypting connections
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { bootstrap } from '@libp2p/bootstrap'
 import { identify } from '@libp2p/identify'
+import { stop } from '@libp2p/interface'
 // Block Store to store DHT data
 const blockstore = new FsBlockstore('block-store')
 const datastore = new FsDatastore('data-store')
@@ -22,40 +23,36 @@ const helia = await createHelia({
     datastore,
     blockstore,
     libp2p: {
-    datastore,
-    blockstore,
-    addresses: {
-      listen: [
-        // add a listen address (localhost) to accept TCP connections on a random port
-        '/ip4/127.0.0.1/tcp/0'
-      ]
-    },
-    transports: [
-      tcp()
-    ],
-    connectionEncryption: [
-      noise()
-    ],
-    streamMuxers: [
-      yamux()
-    ],
-    peerDiscovery: [
-      bootstrap({
-        list: [
-          '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
-          '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-          '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
-          '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
-          '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
-            ]
-      })
-    ],
-    services: {
-      identify: identify()
+      addresses: {
+        listen: [
+          // add a listen address (localhost) to accept TCP connections on a random port
+          '/ip4/127.0.0.1/tcp/0'
+        ]
+      },
+      transports: [
+        tcp()
+      ],
+      connectionEncryption: [
+        noise()
+      ],
+      streamMuxers: [
+        yamux()
+      ],
+      peerDiscovery: [
+        bootstrap({
+          list: [
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+              ]
+        })
+      ],
+      services: {
+        identify: identify()
+      }
     }
-}
-
-
 })
 
 // create a filesystem on top of Helia, in this case it's UnixFS
@@ -93,3 +90,5 @@ for await (const chunk of fs2.cat(cid)) {
 }
 
 console.log('Added file contents:', text)
+
+await stop(helia, helia2)
